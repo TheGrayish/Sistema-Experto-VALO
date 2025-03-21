7cf1157dc7b77bfd32ecf2acf1843b713f3c827f
TheGrayish <yezael01@hotmail.com>
2025-03-20 23:54:49 -0600

    first commit

---
+++ Valorant GOD.pl
@@ -0,0 +1,44 @@
+
+%Mapas Disponibles
+
+mapa(ascent).
+mapa(bind).
+mapa(haven).
+mapa(split).
+mapa(icebox).
+mapa(fracture).
+mapa(pearl).
+mapa(lotus).
+mapa(sunset).
+
+%Agenetes  y Roles
+
+% Duelistas
+duelista(jett).
+duelista(reyna).
+duelista(raze).
+duelista(neon).
+duelista(phoenix).
+duelista(yoru).
+
+% Iniciadores
+iniciador(sova).
+iniciador(breach).
+iniciador(kayo).
+iniciador(skye).
+iniciador(fade).
+iniciador(gekko).
+
+% Controladores
+controlador(brimstone).
+controlador(omen).
+controlador(viper).
+controlador(astra).
+controlador(harbor).
+
+% Centinelas
+centinela(cypher).
+centinela(killjoy).
+centinela(sage).
+centinela(chamber).
+centinela(deadlock).
